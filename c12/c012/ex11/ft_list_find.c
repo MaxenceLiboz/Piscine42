6190a64852f4d4bd60db482520dddd9e7f2b8c86
Maxence <maxoulib@gmail.com>
2021-08-30 14:29:46 +0200

    Rename

---
+++ c12/c012/ex11/ft_list_find.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_find.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mliboz <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/24 12:47:56 by mliboz            #+#    #+#             */
+/*   Updated: 2021/08/24 13:07:17 by mliboz           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_list.h"
+
+t_list	*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
+{
+	while (begin_list->next)
+	{
+		if ((*cmp)(begin_list->data, data_ref) == 0)
+			return (begin_list);
+		begin_list = begin_list->next;
+	}
+	return (begin_list);
+}
