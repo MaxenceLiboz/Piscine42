09e0c637469e1c6de63bb4f2a2f284ff10e9d9f6
Maxence <maxoulib@gmail.com>
2021-09-01 17:19:00 +0200

    Chngs

--- c12/ex12/ft_list_remove_if.c
+++ c12/ex12/ft_list_remove_if.c
@@ -3,22 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_list_remove_if.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mliboz <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: maxenceliboz <maxenceliboz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 13:04:04 by mliboz            #+#    #+#             */
-/*   Updated: 2021/08/25 09:35:51 by mliboz           ###   ########lyon.fr   */
+/*   Updated: 2021/09/01 16:35:32 by maxencelibo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "ft_list.h"
 #include <stdlib.h>
 
-void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *))
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(),
+		void (*free_fct)(void *))
 {
 	t_list	*temp;
 	t_list	*head;
 
 	head = *begin_list;
-	while ((*cmp)(head->next->data, data_ref) != 0)
+	while ((*cmp)(head->data, data_ref) != 0)
 		head = head->next;
 	temp = head;
 	head = head->next;
