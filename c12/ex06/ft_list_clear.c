7fc03fce49025ec91050a01150ed0d7341592d64
Maxence <maxoulib@gmail.com>
2021-09-01 15:48:03 +0200

    Clear list

--- c12/ex06/ft_list_clear.c
+++ c12/ex06/ft_list_clear.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_list_clear.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mliboz <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: maxenceliboz <maxenceliboz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/24 08:15:45 by mliboz            #+#    #+#             */
-/*   Updated: 2021/08/24 08:21:51 by mliboz           ###   ########lyon.fr   */
+/*   Created: 2021/08/30 18:32:34 by maxencelibo       #+#    #+#             */
+/*   Updated: 2021/09/01 15:47:30 by maxencelibo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
 		temp = begin_list;
 		begin_list = begin_list->next;
 		(*free_fct)(temp->data);
+		free(temp->next);
 		free(temp);
 	}
 }
